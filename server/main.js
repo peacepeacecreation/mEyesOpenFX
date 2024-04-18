e4f19671f8a3e77b6d543698b705783bebc56b37
peacepeacecreation <peacepeacecreation@gmail.com>
2024-04-18 10:20:03 +0300

    [SERV] remove cors options

--- server/main.js
+++ server/main.js
@@ -4,13 +4,13 @@ import { filterMessage } from './messages.js'
 
 const server = express()
 
-var corsOptions = {
-    origin: 'http://185.237.206.20',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// var corsOptions = {
+//     origin: 'http://185.237.206.20',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
 
-server.use(cors(corsOptions))
+server.use(cors())
 
 server.get('/messages', async (req, res) => {
     const data = await filterMessage({ from: 'Nervas' })
