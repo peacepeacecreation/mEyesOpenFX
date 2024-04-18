8ba615bb007c5b8fb0cb08b69ee534a0ccd49752
peacepeacecreation <peacepeacecreation@gmail.com>
2024-04-18 10:17:08 +0300

    [SERV] cors option

--- server/main.js
+++ server/main.js
@@ -1,7 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 import { filterMessage } from './messages.js'
 
 const server = express()
+
+var corsOptions = {
+    origin: 'http://185.237.206.20',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+
+server.use(cors(corsOptions))
 
 server.get('/messages', async (req, res) => {
     const data = await filterMessage({ from: 'Nervas' })
